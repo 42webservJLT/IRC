3a6937954f3b634ae4674264bf12c4dd8b8326ca
LeondavidZipp <leondavidzipp@gmx.de>
2025-01-07 13:24:18 +0100

    adjusted main && main hpp

---
+++ inc/main.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Leon David Zipp on 1/7/25.
+//
+
+#ifndef IRC_MAIN_H
+#define IRC_MAIN_H
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <cstdint>
+
+#endif //IRC_MAIN_H
--- src/main.cpp
+++ src/main.cpp
@@ -11,15 +11,21 @@ int main(int argc, char **argv) {
 	}
 
 	try {
-		size_t port = std::stoi(argv[1]);
+//		get port & password from command line arguments
+		size_t portSizeT = std::stoul(argv[1]);
+		if (portSizeT > UINT16_MAX) {
+			throw std::out_of_range("Port number out of range for uint16_t");
+		}
+		uint16_t port = static_cast<uint16_t>(portSizeT);
 		std::string password = argv[2];
+
+//		create server instance & run
+		Server server(port, password);
+		server.run();
 	} catch (std::exception &e) {
-		std::cerr << "Invalid arguments: " << e.what() << std::endl;
+		std::cerr << e.what() << std::endl;
 		return 1;
 	}
-
-	Server server(port, password);
-	server.run();
 
 	return 0;
 }
