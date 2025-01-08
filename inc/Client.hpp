e0e8368536cd698c8f52106a907a0a70106987d8
LeondavidZipp <leondavidzipp@gmx.de>
2025-01-08 10:49:17 +0100

    formatting + getters + setters declarations && corresponding implementations

--- inc/Client.hpp
+++ inc/Client.hpp
@@ -5,25 +5,36 @@
 #ifndef IRC_CLIENT_H
 #define IRC_CLIENT_H
 
+#include <string>
+#include <vector>
 
-typedef ClientState enum {
+
+typedef enum {
 //	client is connected, but not yet authenticated
 	CONNECTED,
 //	client is connected & authenticated
 	AUTHENTICATED,
-};
+} ClientState;
 
 class Client {
 	public:
 		Client();
 		~Client();
 
+		std::string GetUserName() const;
+		std::string GetNickName() const;
+		bool GetAuthenticated() const;
+
+//		TODO: implement
+		void SetUserName(std::string userName);
+		void SetNickName(std::string nickName);
+		void SetAuthenticated(bool authenticated);
+//		TODO: end
+
 	private:
 		std::string _userName;
 		std::string _nickName;
-		std::vector<std::string> _channels;
 		bool _authenticated;
-		std::vector<std::string> _joinedChannels;
 };
 
 
--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -2,13 +2,18 @@
 // Created by Leon David Zipp on 1/7/25.
 //
 
-#include "../../inc/Client.hpp"
+#include "Client.hpp"
 
-
+/* --------------------------------------------------------------------------------- */
+/* Constructors & Destructors                                                        */
+/* --------------------------------------------------------------------------------- */
 Client::Client() : _userName(""), _nickName(""), _authenticated(false) {}
 
 Client::~Client() {}
 
+/* --------------------------------------------------------------------------------- */
+/* Getters & Setters                                                                 */
+/* --------------------------------------------------------------------------------- */
 // returns the username of the client
 std::string Client::GetUserName() const {
 	return _userName;
@@ -20,12 +25,21 @@ std::string Client::GetNickName() const {
 }
 
 // returns if client is authenticated
-bool Client::IsAuthenticated() const {
+bool Client::GetAuthenticated() const {
 	return _authenticated;
 }
 
-// returns the channel names of channels the client has joined
-std::vector<std::string> Client::GetJoinedChannels() const {
-	return _joinedChannels;
+// sets the username of the client
+void Client::SetUserName(std::string userName) {
+	_userName = userName;
 }
 
+// sets the nickname of the client
+void Client::SetNickName(std::string nickName) {
+	_nickName = nickName;
+}
+
+// sets if client is authenticated
+void Client::SetAuthenticated(bool authenticated) {
+	_authenticated = authenticated;
+}
