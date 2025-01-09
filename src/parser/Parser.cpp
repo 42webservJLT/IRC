bface3966d0f56634529cee5de4ca2941aa5c188
Jonas Götz <jonas@42heilbronn.de>
2025-01-09 20:23:50 +0100

    detect method

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -17,4 +17,15 @@ std::tuple<Method, std::vector<std::string>&> Parser::parse(std::string& msg) co
     if (std::getline(tokenStream, token, ' ')) {
         // Convert to uppercase for case-insensitive comparison
         std::transform(token.begin(), token.end(), token.begin(), ::toupper);
+
+        if (token == "AUTHENTICATE") method = AUTHENTICATE;
+        else if (token == "NICK") method = NICK;
+        else if (token == "USER") method = USER;
+        else if (token == "JOIN") method = JOIN;
+        else if (token == "PRIVMSG") method = PRIVMSG;
+        else if (token == "KICK") method = KICK;
+        else if (token == "INVITE") method = INVITE;
+        else if (token == "TOPIC") method = TOPIC;
+        else if (token == "MODE") method = MODE;
+        else method = INVALID;
 }
