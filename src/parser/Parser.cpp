4924fda55e18678b4fea4833c96b28d31bc6dafe
Jonas Götz <jonas@42heilbronn.de>
2025-01-09 20:22:52 +0100

    format

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -7,6 +7,7 @@ Parser::Parser() {
 Parser::~Parser() {
 
 }
+
 std::tuple<Method, std::vector<std::string>&> Parser::parse(std::string& msg) const {
     std::vector<std::string> tokens;
     std::string token;
