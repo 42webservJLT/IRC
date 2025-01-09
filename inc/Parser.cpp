eebb7e027d82866cacba1d6f4f579f2cc9428477
Jonas Götz <jonas@42heilbronn.de>
2025-01-09 20:25:05 +0100

    parser hpp

---
+++ inc/Parser.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Jonas Götz on 1/9/25.
+//
+
+#ifndef IRC_PARSER_H
+#define IRC_PARSER_H
+
+#include <string>
+#include <vector>
+#include "Client.hpp"
+
+class Channel {
+	public:
+		Parser();
+		~Parser();
+
+//		TODO: implement
+		std::tuple<Method, std::vector<std::string>&> parse(std::string& msg) const;
+//		TODO: end
+
+	private:
+};
+
+
+#endif //IRC_PARSER_H
