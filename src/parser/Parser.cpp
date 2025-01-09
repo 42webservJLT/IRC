66de3bc55cd66ea5c41622d72f43f146f303c4ff
Jonas Götz <jonas@42heilbronn.de>
2025-01-09 20:22:32 +0100

    variables for parsing

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -8,4 +8,8 @@ Parser::~Parser() {
 
 }
 std::tuple<Method, std::vector<std::string>&> Parser::parse(std::string& msg) const {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(msg);
+    Method method = INVALID;
 }
