f704de062a3c245849763bad5024225027a7fe5e
LeondavidZipp <leondavidzipp@gmx.de>
2025-01-13 15:11:55 +0100

    readded enums (see previous commit)

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -4,6 +4,8 @@
 
 #include "Server.hpp"
 
+Mode _strToModeEnum(std::string str);
+
 /* --------------------------------------------------------------------------------- */
 /* Constructors & Destructors                                                        */
 /* --------------------------------------------------------------------------------- */
@@ -449,4 +451,31 @@ int Server::_findClientFromNickname(std::string nickname) {
 		}
 	}
 	return -1;
+}
+
+// converts a string to a mode enum
+Mode _strToModeEnum(std::string str) {
+	if (str == "+i") {
+		return MAKE_INVITE_ONLY;
+	} else if (str == "-i") {
+		return UNMAKE_INVITE_ONLY;
+	} else if (str == "+t") {
+		return MAKE_TOPIC_ONLY_SETTABLE_BY_OPERATOR;
+	} else if (str == "-t") {
+		return UNMAKE_TOPIC_ONLY_SETTABLE_BY_OPERATOR;
+	} else if (str == "+o") {
+		return GIVE_OPERATOR_PRIVILEGES;
+	} else if (str == "-o") {
+		return TAKE_OPERATOR_PRIVILEGES;
+	} else if (str == "+l") {
+		return SET_USER_LIMIT;
+	} else if (str == "-l") {
+		return UNSET_USER_LIMIT;
+	} else if (str == "+k") {
+		return SET_PASSWORD;
+	} else if (str == "-k") {
+		return UNSET_PASSWORD;
+	} else {
+		return INVALID_MODE;
+	}
 }
